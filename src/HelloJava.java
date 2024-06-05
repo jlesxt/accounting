1ab9b173a2bc0412928f2e481528e3827350df64
itpsystem1 <itpsystem1@gmail.com>
2024-06-05 16:09:48 +0700

    commit from itpsystem1

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Java");
+System.out.println("Hello Java - new commit from ITPSYSTEM1 20240605");
 	}
 
 }
