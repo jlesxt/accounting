ec9f0e6d517b09f5c89cc9c8cf20f2fa4138e03d
Asrock <Asrock@DESKTOP-JP4DFF6>
2024-06-06 22:45:32 +0700

    commit jlesxt # 2

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Java - new commit from ITPSYSTEM1 20240605");
+System.out.println("Hello Java - new commit from JLESXT 20240606");
 	}
 
 }
